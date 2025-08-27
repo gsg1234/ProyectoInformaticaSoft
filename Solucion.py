00732d954aeb437113ff606c581a657ace514b00
gsg1234 <gonzalogantus@hotmail.com>
2025-08-27 15:03:22 -0300

    Standard Firmata

--- Solucion.py
+++ Solucion.py
@@ -13,9 +13,22 @@ panel = PanelLuces()
 def control_luces_externo():
     while panel.running:
         ########bucle principal##########
+        if panel.button_pressed:
+            panel.encender_luz("verde",True)
+        else:
+            panel.encender_luz("verde",False)
         time.sleep(0.001)
 
+def control_luces_2():
+    while panel.running:
+        panel.encender_luz("roja",True)
+        time.sleep(1)
+        panel.encender_luz("roja",False)
+        time.sleep(1)
+
 hilo_externo = threading.Thread(target=control_luces_externo, daemon=True)
+hilo_externo2 = threading.Thread(target=control_luces_2, daemon=True)
 hilo_externo.start()
+hilo_externo2.start()
 
 panel.run()
